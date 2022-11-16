f8065ec3e712d0b8da32c80b4bf7f312ff3137be
Lilian <munteanlilian@gmail.com>
2022-11-16 17:52:41 +0000

    useDispatch

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
-import {auth} from './firebase'
+import { auth } from './firebase';
+import { useDispatch } from "react-redux";
+import { login, logout } from "./features/userSlice";
 
 function App() {
   const user = null;
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        // Logged in
-        // console.log(userAuth);
+        dispatch(
+          login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
       } else {
-        // Logged out
+        dispatch(logout)
       }
     });
 
