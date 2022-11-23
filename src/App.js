2a379a8bdae52bc6951f48d6cee499e2372adbd2
Lilian <munteanlilian@gmail.com>
2022-11-23 23:30:54 +0200

    ProfileScreen comps created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import { auth } from './firebase';
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout, selectUser } from "./features/userSlice";
@@ -35,6 +36,7 @@ function App() {
           ) : ( 
           <Routes>
             <Route exact path="/" element={<HomeScreen />} />
+            <Route exact path="/profile" element={<ProfileScreen />} />
           </Routes>
           )}
         </Router>
