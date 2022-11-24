c2bdfc6b6438003841f5871e5b2366df8aaed35b
Lilian <munteanlilian@gmail.com>
2022-11-24 02:39:48 +0200

    innit

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ function App() {
           email: userAuth.email,
         }))
       } else {
-        dispatch(logout)
+        dispatch(logout())
       }
     });
 
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
   
   return (
     <div className="app">
