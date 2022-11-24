3e10832286b8d5bc2aa55898ae033c6de08e070f
Lilian <munteanlilian@gmail.com>
2022-11-24 02:39:38 +0200

    ProfileScreen added

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import Nav from '../Nav';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import { auth } from '../firebase';
 
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
+    const user = useSelector(selectUser);
+
   return (
     <div className='profileScreen'>
         <Nav />
+        <div className='profileScreen_body'>
+            <h1>Edit Profile</h1>
+            <div className='profileScreen_info'>
+            <img 
+                src="https://i.pinimg.com/736x/c4/88/34/c488340ad56e5454f4576f6c708b63aa.jpg"
+                alt=""
+            />
+            <div className='profileScreen_details'>
+                <h2>{user.email}</h2>
+                <div className='profileScreen_plans'>
+                    <h3>Plans</h3>
+                    <PlansScreen />
+                    <button
+                        onClick={() => auth.signOut()} 
+                        className='profileScreen_signOut'
+                    >
+                        Sign Out
+                    </button>
+                </div>
+            </div>
+            </div>
+        </div>
     </div>
   )
 }
