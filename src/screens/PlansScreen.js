c66651e9366a9626d5cb96d5878148f556d5040f
Lilian <munteanlilian@gmail.com>
2022-11-28 20:18:26 +0200

    solved subscribe redirect button

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import db from '../firebase';
-import { collection, getDocs, where, query, onSnapshot, addDoc, getDoc, doc } from 'firebase/firestore';
+import { collection, getDocs, where, query, onSnapshot, addDoc, setDoc  } from 'firebase/firestore';
 
 import './PlansScreen.css';
 
@@ -11,58 +11,68 @@ import { loadStripe } from '@stripe/stripe-js';
 function PlansScreen() {
     const [products, setProducts] = useState([]);
     const user = useSelector(selectUser);
-
+  
     useEffect(() => {
-        const q = query(collection(db, "products"), where("active", "==", true));
-        const unsub = onSnapshot(q, (querySnapshot) => {
+        const fetchData = async () => {
+            const q = query(collection(db, "products"), where("active", "==", true));
+            const querySnapshot = await getDocs(q);
             const products = {};
-            querySnapshot.forEach(async (productDoc) => {
-                products[productDoc.id] = await productDoc.data();
 
-                const priceSnap = await getDocs(collection(productDoc.ref, "prices"));
-                priceSnap.docs.forEach((price) => {
-                    products[productDoc.id].price = {
-                        priceId: price.id,
-                        priceData: price.data(),
-                    };
-                });
+            const data = querySnapshot.forEach(async (productDoc) => {
+              products[productDoc.id] = productDoc.data();
+
+              const subQuery = query(
+                collection(db, `products/${productDoc.id}/prices`)
+              );
+              const subQDetails = await getDocs(subQuery);
+              subQDetails.docs.map((price) => {
+                products[productDoc.id].prices = {
+                  priceId: price.id,
+                  priceData: price.data(),
+                };
+              });
+              setProducts(products); 
             });
-            setProducts(products);
-        });
+          };
 
-    }, []);
+          fetchData()
+      }, []);
+    
+        const loadCheckout = async (priceId) => {
+            const firstQ = collection(db, `customers/${user.uid}/checkout_sessions`);
+            const secondQ = query(
+                await addDoc(firstQ, {
+                    price: priceId,
+                    success_url: window.location.origin,
+                    cancel_url: window.location.origin,
+          })
+        );
 
-    const loadCheckout = async (priceId) => {
-        const docRef = doc(db, "customers", `${user?.uid}`);
-        const docSnap = await getDoc(docRef);
-        const addedRef = await addDoc(collection(docRef, "checkout_sessions"), {
-        price: priceId,
-        success_url: window.location.origin,
-        cancel_url: window.location.origin,
-      })
-
-        addedRef.onSnapshot(async (snap) => {
+        onSnapshot(secondQ, async (snap) => {
             const { error, sessionId } = snap.data();
-
+      
             if (error) {
-                alert(`An error has occured: ${error.message}`);
+              alert(`An error occured: ${error.message}`);
+              console.log(error.message);
             }
-
             if (sessionId) {
-                const stripe = await loadStripe('pk_test_51M7RADImyi2sulgfU2tqd1j6R4UOPERtrm5VUBvquWJcvSItxcSfpnWOuE5Mjf2RYJLVympo7FqJwRhgEJ5sqn5w0085LzZDAc');
-                stripe.redirectToCheckout({ sessionId });
+              const stripe = await loadStripe(
+                "pk_test_51M7RADImyi2sulgfU2tqd1j6R4UOPERtrm5VUBvquWJcvSItxcSfpnWOuE5Mjf2RYJLVympo7FqJwRhgEJ5sqn5w0085LzZDAc"
+              );
+              stripe.redirectToCheckout({ sessionId });
             }
-        });
-    };
+          });
+        };
 
   return (
     <div className='plansScreen'>
         {Object.entries(products).map(([productId, productData]) => {
             return (
-                <div className='plansScreen_plan'>
+                <div className='plansScreen_plan' key={productId}>
                     <div className='plansScreen_info'>
                         <h5>{productData.name}</h5>
                         <h6>{productData.description}</h6>
+                        <h6>{productData?.prices?.priceId}</h6>
                     </div>
 
                     <button onClick={() => loadCheckout(productData.prices.priceId)}>
